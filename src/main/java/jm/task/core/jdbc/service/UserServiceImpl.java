7a88d286f953e2c9db61f86abfc258be7edbcc3f
Islam Dzeytov <dzeytov.is@yandex.ru>
2022-02-09 23:00:16 +0300

    .

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
@@ -13,7 +14,7 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    UserDaoJDBCImpl userService = new UserDaoJDBCImpl();
+    UserDao userService = new UserDaoJDBCImpl();
     public void createUsersTable() {
         userService.createUsersTable();
     }
